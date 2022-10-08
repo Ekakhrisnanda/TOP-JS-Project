c95316a032e2cb4b3e469743076a05f859d355a2
Eduk <ekakhrisnanda@gmail.com>
2022-10-08 11:50:14 +0700

    Make playRound function that store conditional statement for the rules of the game

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ function getComputerChoice() {
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+
+function playRound(playerSelection, compSelection) {
+    if (playerSelection == compSelection) {
+        return "That's a tie";
+    } else if (playerSelection == "Rock") {
+        if (compSelection == "Scissor") {
+            return "OK, you win...";
+        } else {
+            return "You lose haha!";
+        }
+    } else if (playerSelection == "Paper") {
+        if (compSelection == "Rock") {
+            return "OK, you win...";
+        } else {
+            return "You lose haha!";
+        }
+    } else if (playerSelection == "Scissor") {
+        if (compSelection == "Paper") {
+            return "OK, you win...";
+        } else {
+            return "You lose haha!";
+        }
+    }
+}
